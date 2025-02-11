c21ec8143a60a1e44f88af9aa36b8b0ca85d749b
anohis <38350932+anohis@users.noreply.github.com>
2025-02-11 21:56:48 +0800

    fix create instance error

--- Editor/TypeExtensions.cs
+++ Editor/TypeExtensions.cs
@@ -1,11 +1,31 @@
 namespace DrawableMember.Editor
 {
     using System;
+    using System.Linq;
 
     internal static class TypeExtensions
     {
         public static bool IsInheritsFrom(this Type derived, Type @base)
             => derived.IsSubclassOf(@base)
                 || @base.IsAssignableFrom(derived);
+
+        public static object CreateInstance(this Type type)
+        {
+            var constructor = type
+                .GetConstructors()
+                .First();
+            var parameters = constructor
+                .GetParameters()
+                .Select(parameter => parameter.HasDefaultValue
+                    ? parameter.DefaultValue
+                    : parameter.ParameterType.GetDefault())
+                .ToArray();
+            return constructor.Invoke(parameters);
+        }
+
+        public static object GetDefault(this Type type)
+            => type.IsValueType
+                ? Activator.CreateInstance(type)
+                : null;
     }
 }
